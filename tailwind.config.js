b5fb2ef7ee7019e533cdf147a3e9f59fd853f6e8
osugo <josugo26@gmail.com>
2022-10-11 18:02:14 +0300

    Configuring the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    
+    colors:{
+      indigo:{
+        light:'#b3bcf5',
+        DEFAULT:'#5c6ac4',
+        dark:'#202e78',
+      }
+    },
+    extend: {
+      screens:{
+        '2xl':'1600px',
+        
+      },
+      colors:{
+        'regal-blue':'#243c5a',
+      }
+
+    },
   },
   plugins: [],
 }
